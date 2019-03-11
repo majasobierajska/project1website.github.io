4b4486b80e6d930671b71ca0da58efe0b3918713
majasobierajska <46842504+majasobierajska@users.noreply.github.com>
2019-03-11 10:09:09 +0100

    navbar scroll to the contact div

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-//event listener
+// scroll from navbar link to div using vanilla JS
+let offset = 0;
+let call;
+function scroll() {
+    if ((offset - document.documentElement.scrollTop) > 0) {
+        document.documentElement.scrollTop += 20
+    }
+    else {
+        clearInterval(call)
+    }
+};
+// Add Event Listener to parent Element 
+document.querySelector('.navigation-bar').addEventListener("click", reply_click);
+//CallBack Function
+function reply_click(e) {
+    e.preventDefault();
+    call = setInterval(scroll, 20);
+    target = e.srcElement.dataset.scroll;
+    offset = document.getElementById(target).offsetTop
+
+}
+
+//event listener for button
 document.querySelector('.button').addEventListener('click', onClick);
 function onClick()
 {
