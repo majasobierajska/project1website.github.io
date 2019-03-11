fdd49da68015d862677c3f81801413316b099d95
majasobierajska <46842504+majasobierajska@users.noreply.github.com>
2019-03-11 10:58:48 +0100

    added google maps in div, however API doesn't work

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ function reply_click(e) {
 
 }
 
+//google map js
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("map"),mapProp);
+    }
+
 //event listener for button
 document.querySelector('.button').addEventListener('click', onClick);
 function onClick()
