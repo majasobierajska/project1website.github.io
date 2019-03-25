ae5427a8293ac89564595c94e390939775839de8
majasobierajska <46842504+majasobierajska@users.noreply.github.com>
2019-03-25 18:23:59 +0100

    added jquery for smooth scroll

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-// scroll from navbar link to div using vanilla JS
-let offset = 0;
-let call;
-function scroll() {
-    if ((offset - document.documentElement.scrollTop) > 0) {
-        document.documentElement.scrollTop += 30
-    }
-    else {
-        clearInterval(call)
-    }
-};
+//smooth scroll using jquery
+$(document).ready(function(){
+    // Add smooth scrolling to links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1000, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
 // Add Event Listener to parent Element 
 document.querySelector('.navigation-bar').addEventListener("click", reply_click);
 //CallBack Function
